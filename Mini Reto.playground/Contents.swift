5b56256019560e3f9faed7d541004b31776bc6d8
carlossru <carlos.znx43@gmail.com>
2016-07-08 10:25:17 -0500

    Tarea1
    Curso Swift

---
+++ Mini Reto.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for numero in 0...100 {
+    
+    if (numero % 5) == 0 {
+        print("\(numero) Bingo!!")
+    }
+    if (numero % 2) == 0 {
+        print("\(numero) Par!!")
+    } else {
+        print("\(numero) Impar!!")
+    }
+    if (numero >= 30) && (numero <= 40) {
+        print("\(numero) Viva Swift")
+    }
+    
+}
